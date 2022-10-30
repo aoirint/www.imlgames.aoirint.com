82837c25d0b7a2d127fa496d5236356d0c12845a
aoirint <aoirint@gmail.com>
2022-10-31 06:29:40 +0900

    mcpixelmon page

---
+++ pages/minecraft-java/mcpixelmon-cj/index.tsx
@@ -0,0 +1,67 @@
+import Head from 'next/head'
+import Link from 'next/link'
+
+export default function Home() {
+  return (
+    <div>
+      <Head>
+        <html className='has-navbar-fixed-top' />
+        <title>imlgames.aoirint.com</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <nav className="navbar is-fixed-top">
+        <div className="navbar-menu">
+          <div className="navbar-start">
+            <Link className="navbar-item" href="/">
+              <button className='button'>
+                &lt;
+              </button>
+            </Link>
+          </div>
+        </div>
+      </nav>
+      <section className="section">
+        <div className="container">
+          <h1 className="title">
+            mcpixelmon-cj
+          </h1>
+          <h2 className="subtitle">
+            Minecraft Java版 ゲームサーバ
+          </h2>
+          <div className='content'>
+            <h3>参加方法</h3>
+            <p>a</p>
+            <p>a</p>
+            <p>a</p>
+            <p>a</p>
+            <p>a</p>
+            <p>a</p>
+            <p>a</p>
+            <p>a</p>
+            <p>a</p>
+            <p>a</p>
+            <p>a</p>
+            <p>a</p>
+            <p>a</p>
+            <p>a</p>
+            <p>a</p>
+            <p>a</p>
+            <p>a</p>
+            <p>a</p>
+            <p>a</p>
+            <p>a</p>
+            <p>a</p>
+            <p>a</p>
+            <p>a</p>
+            <p>a</p>
+            <p>a</p>
+            <p>a</p>
+            <p>a</p>
+            <p>a</p>
+            <p>a</p>
+          </div>
+        </div>
+      </section>
+    </div>
+  )
+}

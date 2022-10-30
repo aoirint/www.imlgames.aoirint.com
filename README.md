e3915c421bdab9e4d8b146ec8fc2ce37525ff5ac
aoirint <aoirint@gmail.com>
2022-10-30 11:47:39 +0900

    server list

--- README.md
+++ README.md
@@ -1,3 +1,16 @@
+# www.imlgames.aoirint.com
+
+## Build
+
+```shell
+npm run build
+npm run export
+```
+
+Deploy `./out`.
+
+---
+
 This is a [Next.js](https://nextjs.org/) project bootstrapped with [`create-next-app`](https://github.com/vercel/next.js/tree/canary/packages/create-next-app).
 
 ## Getting Started
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,71 +1,293 @@
 import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
+import Link from 'next/link'
+import { useState } from 'react'
+
+function Main() {
+  return (
+    <section className="section">
+      <div className="container">
+        <h1 className="title">
+          imlgames.aoirint.com
+        </h1>
+        <h2 className="subtitle">
+          Game Servers
+        </h2>
+        <div className='content'>
+          <h3>Minecraft（Java版）</h3>
+          <table className='table'>
+            <thead>
+              <tr>
+                <th>Name</th>
+                <th>Address</th>
+                <th>Server Version</th>
+                <th>Readme</th>
+                <th>Web Map</th>
+                <th>Status</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <td>
+                  mcpixelmon-cj
+                </td>
+                <td>
+                  <code>
+                    mikoto.aoirint.com:25575
+                  </code>
+                </td>
+                <td>
+                  Forge 1.12.2
+                </td>
+                <td>
+                  <Link href="minecraft-java/mcpixelmon-cj/">
+                    Link
+                  </Link>
+                </td>
+                <td>
+                  <Link href="https://mcpixelmon.aoirint.com/">
+                    Link
+                  </Link>
+                </td>
+                <td>
+                  <span className="tag is-success">Running</span>
+                </td>
+              </tr>
+              <tr>
+                <td>
+                  mcbiome-cj
+                </td>
+                <td>
+                  <code>
+                    mikoto.aoirint.com:25576
+                  </code>
+                </td>
+                <td>
+                  Forge 1.19.2
+                </td>
+                <td>
+                  <Link href="minecraft-java/mcbiome-cj/">
+                    Link
+                  </Link>
+                </td>
+                <td>
+                  <Link href="https://mcbiome.aoirint.com/">
+                    Link
+                  </Link>
+                </td>
+                <td>
+                  <span className="tag is-success">Running</span>
+                </td>
+              </tr>
+              <tr>
+                <td>
+                  roka
+                </td>
+                <td>
+                  <code>
+                    mikoto.aoirint.com:25578
+                  </code>
+                </td>
+                <td>
+                  Vanilla 1.19.2
+                </td>
+                <td>
+                  <Link href="minecraft-java/roka/">
+                    Link
+                  </Link>
+                </td>
+                <td>
+                </td>
+                <td>
+                  <span className="tag is-danger">Stopped</span>
+                </td>
+              </tr>
+              <tr>
+                <td>
+                  aur
+                </td>
+                <td>
+                  <code>
+                    mikoto.aoirint.com:25577
+                  </code>
+                </td>
+                <td>
+                  Vanilla 1.19.2
+                </td>
+                <td>
+                  <Link href="minecraft-java/aur/">
+                    Link
+                  </Link>
+                </td>
+                <td>
+                </td>
+                <td>
+                  <span className="tag is-danger">Stopped</span>
+                </td>
+              </tr>
+            </tbody>
+          </table>
+
+          <h3>Minecraft（Bedrock版）</h3>
+          <table className='table'>
+            <thead>
+              <tr>
+                <th>Name</th>
+                <th>Address</th>
+                <th>Server Version</th>
+                <th>Readme</th>
+                <th>Status</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <td>
+                  roka
+                </td>
+                <td>
+                  <code>
+                    mikoto.aoirint.com:19132
+                  </code>
+                </td>
+                <td>
+                  1.19.40.02
+                </td>
+                <td>
+                  <Link href="minecraft-bedrock/roka/">
+                    Link
+                  </Link>
+                </td>
+                <td>
+                  <span className="tag is-danger">Stopped</span>
+                </td>
+              </tr>
+              <tr>
+                <td>
+                  aur
+                </td>
+                <td>
+                  <code>
+                    mikoto.aoirint.com:19134
+                  </code>
+                </td>
+                <td>
+                  1.19.40.02
+                </td>
+                <td>
+                  <Link href="minecraft-bedrock/aur/">
+                    Link
+                  </Link>
+                </td>
+                <td>
+                  <span className="tag is-danger">Stopped</span>
+                </td>
+              </tr>
+            </tbody>
+          </table>
+
+          <h3>ARK: Survival Evolved</h3>
+          <table className='table'>
+            <thead>
+              <tr>
+                <th>Name</th>
+                <th>Address</th>
+                <th>Server Version</th>
+                <th>Readme</th>
+                <th>Status</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <td>
+                  ark-aur
+                </td>
+                <td>
+                  <code>
+                    mikoto.aoirint.com:7777,7778,27015
+                  </code>
+                </td>
+                <td>
+                  9382229 (2022-08-25)
+                </td>
+                <td>
+                  <Link href="ark/ark-aur/">
+                    Link
+                  </Link>
+                </td>
+                <td>
+                  <span className="tag is-danger">Stopped</span>
+                </td>
+              </tr>
+            </tbody>
+          </table>
+
+          <h3>Terraria</h3>
+          <table className='table'>
+            <thead>
+              <tr>
+                <th>Name</th>
+                <th>Address</th>
+                <th>Server Version</th>
+                <th>Readme</th>
+                <th>Status</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <td>
+                  terraria-aur
+                </td>
+                <td>
+                  <code>
+                    mikoto.aoirint.com:17777
+                  </code>
+                </td>
+                <td>
+                  TShock 1.4.3.6
+                </td>
+                <td>
+                  <Link href="terraria/terraria-aur/">
+                    Link
+                  </Link>
+                </td>
+                <td>
+                  <span className="tag is-success">Running</span>
+                </td>
+              </tr>
+              <tr>
+                <td>
+                  terraria-aur2
+                </td>
+                <td>
+                  <code>
+                    mikoto.aoirint.com:17778
+                  </code>
+                </td>
+                <td>
+                  <Link href="terraria/terraria-aur2/">
+                    Link
+                  </Link>
+                </td>
+                <td>
+                  <span className="tag is-success">Running</span>
+                </td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
+      </div>
+    </section>
+  )
+}
 
 export default function Home() {
   return (
-    <div className={styles.container}>
+    <div>
       <Head>
-        <title>Create Next App</title>
-        <meta name="description" content="Generated by create next app" />
+        <title>imlgames.aoirint.com</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
-
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.tsx</code>
-        </p>
-
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h2>Documentation &rarr;</h2>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h2>Learn &rarr;</h2>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/canary/examples"
-            className={styles.card}
-          >
-            <h2>Examples &rarr;</h2>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-            className={styles.card}
-          >
-            <h2>Deploy &rarr;</h2>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
-      </main>
-
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <span className={styles.logo}>
-            <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
-          </span>
-        </a>
-      </footer>
+      {/* <Navbar /> */}
+      <Main />
     </div>
   )
 }
